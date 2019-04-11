1b3ab37f8b9a4e805e20339d2f7f486a3f5fa9a0
Riyo <riyo.perry@gmail.com>
2019-04-10 21:24:30 -0700

    finished factorial method

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,6 +1,16 @@
 # Computes factorial of the input number and returns it
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) where n is the value of the input number
+# Space complexity: O(1)
 def factorial(number)
-  raise NotImplementedError
+  if number == nil
+    raise ArgumentError
+  end
+
+  total = 1
+
+  until number == 0
+    total *= number
+    number -= 1
+  end
+  return total
 end
